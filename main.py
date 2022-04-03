c3fac8972bf9e7cde9675d49367e7868848578d3
Kristal1ik <korobovtsevaolga@yandex.ru>
2022-04-03 21:52:36 +0300

    Подключение

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/8")
+def index():
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(
+            (News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 @app.route('/')
 def start():
-    return "Начало грандиозного..."
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.1')
